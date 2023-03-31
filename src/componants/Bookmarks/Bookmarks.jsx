260d505be925bf9858570f9331104dcd74ad88c5
Mostakimw <scientist.expert@gmail.com>
2023-03-31 23:23:59 +0600

    bookmark length and padding fixed

--- src/componants/Bookmarks/Bookmarks.jsx
+++ src/componants/Bookmarks/Bookmarks.jsx
@@ -4,9 +4,11 @@ const Bookmarks = ({ blogTitle, bookmarks }) => {
   return (
     <div className="bg-[#1111110D] mt-5 rounded-md">
       <div className="p-4">
-        <h3 className="text-xl font-bold pb-4">Bookmarked Blogs : 0</h3>
+        <h3 className="text-xl font-bold pb-4">
+          Bookmarked Blogs : {bookmarks ? bookmarks.length : 0}
+        </h3>
         {bookmarks.map((bookmark) => (
-          <p key={bookmark.id} className="bg-base-100 rounded-md p-3">
+          <p key={bookmark.id} className="bg-base-100 rounded-md p-3 mb-4">
             {bookmark.title}
           </p>
         ))}
--- src/componants/Readtime/Readtime.jsx
+++ src/componants/Readtime/Readtime.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 const Readtime = ({ readTime }) => {
-  const [watchTime, setWatchTime] = useState(readTime);
+  const [totalReadTime, setTotalReadTime] = useState(readTime);
   useEffect(() => {
     const getTimeFromLocal = localStorage.getItem("readTime") || 0;
-    setWatchTime(getTimeFromLocal);
+    setTotalReadTime(getTimeFromLocal);
   }, [readTime]);
   return (
     <div className="bg-[#6047EC1A] rounded-md">
       <h2 className="text-[#6047EC] text-xl font-bold text-center py-3 ">
-        Spent time on read : {watchTime} min
+        Spent time on read : {totalReadTime} min
       </h2>
     </div>
   );
